e5ba153b56211298542b6ce806009ff81868c5ba
Laurence Earl <84546222+learlfraser@users.noreply.github.com>
2022-05-05 01:13:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print("Mr Earl")
+name = "Mr Earl"
+print(name)
+fruit = "Banana"
+print(fruit)
+print("fruit")
+
+print("Mr Earl is a " + fruit)
+
+name = name.upper()
+print(name)
+
